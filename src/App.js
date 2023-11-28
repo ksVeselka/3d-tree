a616aeb1c0af55db81ad334cfb8e2b88794965ee
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-11-29 00:39:48 +0200

    add a tree model

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { CameraControls, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Tree } from "./Tree";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Canvas>
+        <OrbitControls />
+        <CameraControls
+          autoRotate
+          enableZoom={false}
+          enablePan={false}
+          enableDamping
+          dampingFactor={1}
+          rotateSpeed={0.5}
+          minPolarAngle={1.3}
+          maxPolarAngle={0}
+          minDistance={4}
+          maxDistance={6}
+          target={[0, 0, 0]}
+          // position={[10, 20, 0]}
+        />
+        <PerspectiveCamera 
+        makeDefault position={[0, 3, 5]} 
+        fov={60} />
+        {/* <ambientLight 
+          intensity={10}
+        /> */}
+        <pointLight position={[5, 5, 5]} intensity={100} />
+        <pointLight position={[0, 0, -15]} intensity={100} />
+        <pointLight position={[-10, 10, 5]} intensity={1000} />
+        {/* <mesh>
+          <boxGeometry
+          args={[2, 1, 1]}
+          position={[0, 0, 0]}
+          />
+          <meshStandardMaterial color={'#9a3be2'} />
+        </mesh> */}
+        <Tree />
+      </Canvas>
     </div>
   );
 }
